73b66bde4e6120cbec5d0387e46aa5c8b550bcb3
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-14 22:18:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,6 +10,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name][key]
           final_array[name][key] = []
         end
+        final_array[name][key].push(k)
       binding.pry
       end
     end
