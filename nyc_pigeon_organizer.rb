e2ada15b7960e23c1df8c5d7b595a621dcd15a9e
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-14 22:17:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,6 +7,9 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] ={}
         end
+        if !final_array[name][key]
+          final_array[name][key] = []
+        end
       binding.pry
       end
     end
